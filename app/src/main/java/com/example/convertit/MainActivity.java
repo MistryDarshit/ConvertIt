e77aac6913f45185aa238c688b2731ea71f114f6
Darshit <darshitmistry2@gmail.com>
2023-02-19 11:22:14 +0530

    App was created and working  lets enjoy it

---
+++ app/src/main/java/com/example/convertit/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.convertit;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private Button  button;
+    private TextView textView;
+    private EditText editText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = findViewById(R.id.button);
+        textView = findViewById(R.id.textView);
+        editText = findViewById(R.id.editText);
+//        Method 1 --->
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//            Toast.makeText(MainActivity.this, "Thanks For Using This App", Toast.LENGTH_SHORT).show();
+//               String s  = editText.getText().toString();
+//               int kg  = Integer.parseInt(s);
+//               double pound = 2.205 * kg;
+//               textView.setText("The corusponding value in pounds is " + pound);
+//            }
+//        });
+    }
+//    Method 2 --->
+    public void calc ( View view){
+        String s  = editText.getText().toString();
+        int kg  = Integer.parseInt(s);
+        double pound = 2.205 * kg;
+        textView.setText("The corusponding value in pounds is " + pound);
+        Toast.makeText(this, "Thanks For Using", Toast.LENGTH_SHORT).show();
+    }
+}
